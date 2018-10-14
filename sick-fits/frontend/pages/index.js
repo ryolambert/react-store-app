9f7af60a770f5ed2d3ddf8c9c5d5648cb4f2bc2d
Ryo Lambert <ryolambert@gmail.com>
2018-10-14 17:10:11 -0600

    next.js custom _app.js layout setup

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+
 const Home = props => (
   <div>
     <p>Hey!</p>
-    <a href="/sell">Sell!</a>
+    <Link href="/sell">
+      <a>Sell!</a>
+    </Link>
   </div>
 );
 
