66b5cded63c0b1b1cc51651418f43b5d46974b7a
James R. Lambert <ryolambert@gmail.com>
2018-10-26 11:43:53 -0600

    established same for item query. ‼Current setup is for quick mockup, change over to the commented out variation for custom logic

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,10 +1,19 @@
 //where all of our database calls are going to be made
+// Note: if the query is exactly the same within prisma and yoga with no custom logic needed you can just forward the query from yoga to prisma.
+
+// Note: prisma-binding gives us the ability to query out database
+const { forwardTo } = require('prisma-binding');
+
 const Query = {
-  dogs(parent, args, ctx, info) {
-    global.dogs = global.dogs || [];
-    return global.dogs;
-    // return [{name: 'Kota'}, {name: 'Emi'}];
-  },
+  //helpful in creating a quick mockup, can forward all the apis and work backwards
+    items: forwardTo('db'),
+  // async items(parent, args, ctx, info) {
+  //   // A O.K. to return a promise from a resolver, smart enough to infer but for the sake of consistency, readability, etc. setting it in a variable
+  //   // return ctx.db.query.item();
+  //   console.log('Getter\' Items!!! 🐱‍🏍')
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // },
 };
 
 module.exports = Query;
