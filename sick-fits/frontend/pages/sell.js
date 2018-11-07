eead4d08c81e1bf90f5c81c3b5dbd934fdcc7d33
James R. Lambert <ryolambert@gmail.com>
2018-11-07 12:10:42 -0700

    format: ignore

--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import CreateItem from '../components/CreateItem';
 
+// Currently configured as a stateless componenet as opposed to a React Component
 const Sell = props => (
   <div>
     <CreateItem />
