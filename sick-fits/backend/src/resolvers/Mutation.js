e3336df2d769b2f0b27f1e74fcfcdcf40047c501
James R. Lambert <ryolambert@gmail.com>
2018-11-07 12:08:35 -0700

    fixed mutation resolver for updateItem

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -18,19 +18,16 @@ const mutations = {
     // Then remove the ID from the updates
     delete updates.id;
     // Finally, we run the update
-    return ctx.db.mutation.updateItem;
-  }
-
-  // createDog(parent, args, ctx, info) {
-  //   // DO NOT DO THIS IN PRODUCTION, just testing around the logic and params
-  //   global.dogs = global.dogs || [];
-  //   // creates the dog here line below is just seeing if the mutation is taking
-  //   const newDog = { name: args.name };
-  //   global.dogs.push(newDog);
-  //   return newDog;
-  //   // console.log(args);
-    
-  // },
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        wehre: {
+          id: args.id,
+        },
+      },
+      info
+    );
+  },
 };
 
 module.exports = mutations;
