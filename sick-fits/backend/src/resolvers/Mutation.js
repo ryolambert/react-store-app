dd10f90201150e6edd2cff44c8897c900f11cec7
James R. Lambert <ryolambert@gmail.com>
2018-11-07 17:25:36 -0700

    created deleteItem Resolver within our mutators

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -28,6 +28,15 @@ const mutations = {
       info
     );
   },
+  deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`);
+    // 2. Check if they own that item, or have the permissions'
+    // TODO
+    // 3. Delete it 💥
+    return ctx.db.mutation.deleteItem({ where }, info);
+  }
 };
 
 module.exports = mutations;
