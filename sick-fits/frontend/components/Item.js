1259e532882501e48b01fd70d23e39ca472460fc
James R. Lambert <ryolambert@gmail.com>
2018-11-07 17:19:09 -0700

    created Delete Item Component and placed it within the item component

--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -5,6 +5,7 @@ import Title from "./styles/Title";
 import ItemStyles from "./styles/ItemStyles";
 import PriceTag from "./styles/PriceTag";
 import formatMoney from "../lib/formatMoney";
+import DeleteItem from './DeleteItem';
 
 export default class Item extends Component {
   static propTypes = {
@@ -45,7 +46,7 @@ export default class Item extends Component {
             <a>Edit ✏</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
