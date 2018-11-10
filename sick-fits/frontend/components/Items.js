7dea068b35948408718ccf702b0e0c4ac6cf623e
James R. Lambert <ryolambert@gmail.com>
2018-11-10 15:49:32 -0700

    modified our Items.js Component such that our query is kept track of and cached for pagination.

--- sick-fits/frontend/components/Items.js
+++ sick-fits/frontend/components/Items.js
@@ -1,15 +1,19 @@
-import React, { Component } from 'react'
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
-import styled from 'styled-components';
-import Item from './Item';
-import Pagination from './Pagination';
+// NPM Imports
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import styled from "styled-components";
+// Relative Imports
+import Item from "./Item";
+import Pagination from "./Pagination";
+// ‼ Because perPage is a named export it needs the curly braces, if it were a default export then this wouldn't be an issu
+import { perPage } from "../config";
 
 // Best practice to put all of your queries in all Caps with underscores
 // The way we use this query is through Render Prop
 const ALL_ITEMS_QUERY = gql`
-  query ALL_ITEMS_QUERY {
-    items {
+  query ALL_ITEMS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
+    items(first: $first, skip: $skip, orderBy: createdAt_DESC) {
       id
       title
       price
@@ -22,7 +26,7 @@ const ALL_ITEMS_QUERY = gql`
 
 const Center = styled.div`
   text-align: center;
-  `;
+`;
 
 const ItemsList = styled.div`
   display: grid;
@@ -37,27 +41,37 @@ class Items extends Component {
   render() {
     return (
       <Center>
-        <Pagination page={this.props.page}/>
-          {/* The only allowable child of a <Query> Component is a function.
+        <Pagination page={this.props.page} />
+        {/* The only allowable child of a <Query> Component is a function.
           Now that function delivers what's called a payload.
           ES6 note: on the arrow function the parenthesis (payload => line 32) is uneccessary for something with only one argument, if there were multiple then the () would be needed.
 
         Now, destructured payload out into data, error and loading to avoid having to redo each piece */}
-        <Query query={ALL_ITEMS_QUERY}>
-          {({ data, error, loading}) => {
-            if(loading) return <p>Loading...</p>
-            if(error) return <p>Error: {error.message}</p>
+        <Query query={ALL_ITEMS_QUERY}
+        // ‼ One way to solve refreshing page cache after adding to the database to display items, but the biggest issue is with fetchPolicy network only we're having to constantly redownload each page again slowing down our page loads and taking away our ability to cache previous pages.
+        // ‼ Or use refetch on our mutation in CreateItem, but again we run into another issue of possibly having to again run more calculations and requests per item, which would slow performance if hundreds of items were listed. 
+        // ‼ Currently as it stands Apollo does not have an option to for setting parameters/logic on partial caches like timers, etc.
+        // fetchPolicy="network-only"
+        variables={{
+          // setting up query with skip and first to display out the proper number or items defined by perPage and have it display out that number or skip per page left
+          skip: this.props.page * perPage - perPage, 
+        }}>
+          {({ data, error, loading }) => {
+            if (loading) return <p>Loading...</p>;
+            if (error) return <p>Error: {error.message}</p>;
             return (
               // This is how you loop over things in React, you take an array of things and map over it and for each item of the array return something else
               <ItemsList>
-                {data.items.map(item => <Item item={item} key={item.id}/>)}
+                {data.items.map(item => (
+                  <Item item={item} key={item.id} />
+                ))}
               </ItemsList>
             );
           }}
         </Query>
-        <Pagination page={this.props.page}/>
+        <Pagination page={this.props.page} />
       </Center>
-    )
+    );
   }
 }
 
