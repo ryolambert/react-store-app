74f3a3473770a4b62d9691bf52587567e305ba97
James R. Lambert <ryolambert@gmail.com>
2018-11-10 14:18:21 -0700

    added in our next.js links for the pagination component so we can switch over to each page without having to type it into the url. TODO: Get our items pages to display/keep track of the actual items listed per page.

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -1,8 +1,12 @@
+// NPM Imports
 import React from "react";
-import PaginationStyles from "./styles/PaginationStyles";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
-import { perPage } from '../config';
+import Head from "next/head";
+import Link from "next/link";
+// Relative Imports
+import PaginationStyles from "./styles/PaginationStyles";
+import { perPage } from "../config";
 
 // Initializing our Pagination Query => connected to our graphQL db
 const PAGINATION_QUERY = gql`
@@ -20,20 +24,51 @@ const PAGINATION_QUERY = gql`
 */
 
 const Pagination = props => (
-    <Query query={PAGINATION_QUERY}>
-      {({ data, loading, error }) => {
-        if (loading) return <p>Loading...</p>;
-        const count = data.itemsConnection.aggregate.count;
-        const pages = Math.ceil(count / perPage);
-        return (
-          <PaginationStyles>
+  <Query query={PAGINATION_QUERY}>
+    {({ data, loading, error }) => {
+      if (loading) return <p>Loading...</p>;
+      // Setting up variables to pass in our page place in the pagination
+      const count = data.itemsConnection.aggregate.count;
+      const pages = Math.ceil(count / perPage);
+      const page = props.page;
+      return (
+        <PaginationStyles>
+          <Head>
+            <title>
+              Sick Fits! - Page {page} of {pages}
+            </title>
+          </Head>
+          <Link
+            // 🌟 AWESOME NOTE: Prefetch doesn't work in dev mode, but does in production. It's a huge boost for performance, by essentially preloading this element (can be used on any element)
+            prefetch
+            href={{
+              pathname: "items",
+              query: { page: page - 1 }
+            }}
+          >
+            <a className="prev" aria-disabled={page <= 1}>
+              ⬅ Prev
+            </a>
+          </Link>
           <p>
             Page {props.page} of {pages}!
           </p>
-          </PaginationStyles>
-        );
-      }}
-    </Query>
+          <p> {count} Items Total</p>
+          <Link
+            prefetch
+            href={{
+              pathname: "items",
+              query: { page: page + 1 }
+            }}
+          >
+            <a className="next" aria-disabled={page >= pages}>
+              Next ➡
+            </a>
+          </Link>
+        </PaginationStyles>
+      );
+    }}
+  </Query>
 );
 
 export default Pagination;
