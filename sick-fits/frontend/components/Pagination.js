e2e92157f6ce76f000fcc9aee6836b663ef59fd0
James R. Lambert <ryolambert@gmail.com>
2018-11-09 18:20:37 -0700

    Base pagination, database connection working

--- sick-fits/frontend/components/Pagination.js
+++ sick-fits/frontend/components/Pagination.js
@@ -1,5 +1,18 @@
-import React from 'react';
-import PaginationStyles from './styles/PaginationStyles';
+import React from "react";
+import PaginationStyles from "./styles/PaginationStyles";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+
+// Initializing our Pagination Query => connected to our graphQL db
+const PAGINATION_QUERY = gql`
+  query PAGINATION_QUERY {
+    itemsConnection {
+      aggregate {
+        count
+      }
+    }
+  }
+`;
 
 /*
   Notes: Setting up a simple stateless functional component that'll paginate our Items.js component
@@ -7,8 +20,10 @@ import PaginationStyles from './styles/PaginationStyles';
 
 const Pagination = props => (
   <PaginationStyles>
-    <p>👋 Pagination Alert!</p>
+    <Query query={PAGINATION_QUERY}>
+      {({ data, loading, error }) => <p>👋 Pagination Count: {data.itemsConnection.aggregate.count}!</p>}
+    </Query>
   </PaginationStyles>
-)
+);
 
 export default Pagination;
