75bd0546e69f3366af590991d84a3e17b5dbbb3c
James R. Lambert <ryolambert@gmail.com>
2018-11-09 17:49:46 -0700

    initializing pagination component

---
+++ sick-fits/frontend/components/Pagination.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PaginationStyles from './styles/PaginationStyles';
+
+/*
+  Notes: Setting up a simple stateless functional component that'll paginate our Items.js component
+*/
+
+const Pagination = props => (
+  <PaginationStyles>
+    <p>👋 Pagination Alert!</p>
+  </PaginationStyles>
+)
+
+export default Pagination;
