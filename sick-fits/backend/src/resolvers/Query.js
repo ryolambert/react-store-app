fa99efa4ca0e7bc6cb9db89ea028f320efd4b013
James R. Lambert <ryolambert@gmail.com>
2018-11-12 13:26:55 -0700

    added in resolver for user query of me on our schema.

--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -2,16 +2,31 @@
 // Note: if the query is exactly the same within prisma and yoga with no custom logic needed you can just forward the query from yoga to prisma.
 
 // Note: prisma-binding gives us the ability to query out database
-const { forwardTo } = require('prisma-binding');
+const { forwardTo } = require("prisma-binding");
 
 const Query = {
-  //helpful in creating a quick mockup, can forward all the apis and work backwards
-  items: forwardTo('db'),
-  item: forwardTo('db'),
-  itemsConnection: forwardTo('db'), 
+  // helpful in creating a quick mockup, can forward all the apis and work backwards
+  items: forwardTo("db"),
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db"),
+  // * Me Method: pulls in user data created in our schema, definied here
+  me: function(parent, args, ctx, info) {
+    // * Current User Check:
+    // Pulls from the context request for user ID if there isn't a logged in User
+    if (!ctx.request.userId) {
+      // ! Returns null because users may not be logged in
+      return null;
+    }
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
   // async items(parent, args, ctx, info) {
-  //   // A O.K. to return a promise from a resolver, smart enough to infer but for the sake of consistency, readability, etc. setting it in a variable
-  //   // return ctx.db.query.item();
+  //  A O.K. to return a promise from a resolver, smart enough to infer but for the sake of consistency, readability, etc. setting it in a variable
+  //   return ctx.db.query.item();
   //   console.log('Getter\' Items!!! 🐱‍🏍')
   //   const items = await ctx.db.query.items();
   //   return items;
