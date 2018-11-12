473aeefa906b50360af1c9177751e71a8d7d05bd
James R. Lambert <ryolambert@gmail.com>
2018-11-12 12:47:47 -0700

    setting up express middleware for current logged in user

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -21,6 +21,20 @@ const server = createServer();
 server.express.use(cookieParser());
 
 // TODO Use express middleware to populate current user
+// Decoding the JWT so we can obtain user Id on each request
+server.express.use((req, res, next) => {
+  /*
+  * Test to see if middleware is functioning
+  console.log("Hey, I'm a middleware");
+  res.send("test");
+  next();
+  */
+  // * 1. Pull token out of request.
+  console.log(req.cookies);
+  const { token } = req.cookies;
+  //  console.log(token);
+  next();
+});
 
 server.start(
   {
