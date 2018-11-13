ebbbea26c15877f34f76c27cd8389c1a4ed692d9
James R. Lambert <ryolambert@gmail.com>
2018-11-13 00:12:17 -0700

    Setting up base user component.

---
+++ sick-fits/frontend/components/User.js
@@ -0,0 +1,31 @@
+//
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+import PropTypes from "prop-types";
+
+const CURRENT_USER_QUERY = gql`
+  me {
+    id
+    email
+    name
+    permissions
+    # TODO: Add cart & orders
+  }
+`;
+
+// * Making user component
+const User = props => (
+  // {...props} allows us to add in additional props if needed
+  <Query {...props} query={CURRENT_USER_QUERY}>
+    {/* Allows us to pass in our payload without having to redefine our query and pass it every time or prop drill. */}
+    {payload => props.children(payload)}
+  </Query>
+);
+
+// Sets our user prop types so the only thing that we must pass as a child is a function
+User.PropTypes = {
+  children: PropTypes.func.isRequired,
+};
+
+export default User;
+export { CURRENT_USER_QUERY };
