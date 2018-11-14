5a521741e3a3b1a47c8f731c4bc54a0cf1184783
Ryo Lambert <ryolambert@gmail.com>
2018-11-14 14:39:49 -0700

    fixed our gql query so user (me) was assigned properly

--- sick-fits/frontend/components/User.js
+++ sick-fits/frontend/components/User.js
@@ -4,12 +4,14 @@ import gql from "graphql-tag";
 import PropTypes from "prop-types";
 
 const CURRENT_USER_QUERY = gql`
+  query {
   me {
     id
     email
     name
     permissions
     # TODO: Add cart & orders
+  }
   }
 `;
 
