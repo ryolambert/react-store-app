dbcab53f48a73cbe3513d0d96d87288ff6f84ef8
James R. Lambert <ryolambert@gmail.com>
2018-11-19 16:23:04 -0700

    Basic formatting, got component to update current user on nav too

--- sick-fits/frontend/components/SignUp.js
+++ sick-fits/frontend/components/SignUp.js
@@ -1,10 +1,10 @@
 // Sign Up Component: renders out and takes in user input for account sign up and passes it to the backend.
-import React, { Component } from "react";
-import { Mutation } from "react-apollo";
-import gql from "graphql-tag";
-import Form from "./styles/Form";
-import Error from "./ErrorMessage";
-import { CURRENT_USER_QUERY } from "./User";
+import React, { Component } from 'react';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+import Form from './styles/Form';
+import Error from './ErrorMessage';
+import { CURRENT_USER_QUERY } from './User';
 
 // Linking over our graphql mutation for SignUp
 const SIGNUP_MUTATION = gql`
@@ -24,9 +24,9 @@ const SIGNUP_MUTATION = gql`
 class SignUp extends Component {
   // Setting up the initial state for our form fields
   state = {
-    name: "",
-    email: "",
-    password: ""
+    name: '',
+    email: '',
+    password: '',
   };
 
   // State save function for form input
@@ -40,7 +40,7 @@ class SignUp extends Component {
         mutation={SIGNUP_MUTATION}
         variables={this.state}
         refetchQueries={[{ query: CURRENT_USER_QUERY }]}
-        >
+      >
         {/* Passing in our signup function from our mutation with our error and loading states */}
         {(signup, { error, loading }) => (
           // Post method setup as a fallback so we aren't exposing user submitted data in our url or don't use a <Form> tag and just use a collection of inputs.
@@ -52,11 +52,22 @@ class SignUp extends Component {
               // assigning a const res for a custom error/success response on intercept if wanted, otherwise needs an await since our event is async
               const res = await signup();
               // setting our initial user state
-              this.setState({ name: "", email: "", password: "" });
-            }}>
+              this.setState({ name: '', email: '', password: '' });
+            }}
+          >
             <fieldset disabled={loading} aria-busy={loading}>
               <h2>Sign Up for an Account</h2>
               <Error error={error} />
+              <label htmlFor="email">
+                Name
+                <input
+                  type="text"
+                  name="name"
+                  placeholder="John Doe"
+                  value={this.state.name}
+                  onChange={this.saveToState}
+                />
+              </label>
               <label htmlFor="email">
                 Email
                 <input
@@ -87,4 +98,4 @@ class SignUp extends Component {
   }
 }
 
-export default SignIn;
+export default SignUp;
