71d26f49523cc67b24b423f08524f28348acbd97
James R. Lambert <ryolambert@gmail.com>
2018-11-19 16:20:32 -0700

    set up nav to display links according whether user is signed in or not.

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,34 +1,39 @@
-import Link from "next/link";
-import NavStyles from "./styles/NavStyles";
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 import User from './User';
 
-
 const Nav = () => (
-  <NavStyles>
-    <User>
-      {({ data: { me } }) => {
-        console.log(me);
-        if (me) return <p>{me.name}</p>;
-        return null;
-      }}
-      {/* The payload has data inside of it already, so we're using a two level destructuring into data then further into me*/}
-    </User>
-    <Link href="/items">
-      <a>Shop</a>
-    </Link>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
-    <Link href="/signup">
-      <a>Signup</a>
-    </Link>
-    <Link href="/orders">
-      <a>Orders</a>
-    </Link>
-    <Link href="/me">
-      <a>Account</a>
-    </Link>
-  </NavStyles>
+  <User>
+    {({ data: { me } }) => (
+      <NavStyles>
+        {/* The payload has data inside of it already, so we're using a two level destructuring into data then further into me */}
+        <Link href="/items">
+          <a>Shop</a>
+        </Link>
+        {/* ⚛ React Tip: "<> </>" React fragment wrapping our jsx- */}
+        {me && (
+          <>
+            <Link href="/sell">
+              <a>Sell</a>
+            </Link>
+            <Link href="/orders">
+              <a>Orders</a>
+            </Link>
+            <Link href="/me">
+              <a>Account</a>
+            </Link>
+          </>
+        )}
+        {!me && (
+          <>
+            <Link href="/signup">
+              <a>Sign In</a>
+            </Link>
+          </>
+        )}
+      </NavStyles>
+    )}
+  </User>
 );
 
 export default Nav;
